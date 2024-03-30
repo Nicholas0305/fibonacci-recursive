387d2660367e4103d0556a8d39fba27cb865e665
Nicholas <najernigin@gmail.com>
2024-03-29 20:12:11 -0500

    Complete Lab

--- javascript/fibonacci_recursive.js
+++ javascript/fibonacci_recursive.js
@@ -1,5 +1,11 @@
-function fibonacci(n) {
-  // type your code here
+function fibonacci(n, a = 0, b = 1) {
+  if (n === 0) {
+    return a;
+  }
+  if (n === 1) {
+    return b;
+  }
+  return fibonacci(n - 1, b, a + b);
 }
 
 if (require.main === module) {
